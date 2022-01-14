e9518e5acd72ea1ff57ba3d2ef38d71b7ec7bde9
Yan Cao <34569648+Giant98@users.noreply.github.com>
2022-01-14 10:39:08 +0800

    添加快速排序

--- 手撸代码/Sort.cpp
+++ 手撸代码/Sort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//√∞≈›≈≈–Ú
 template<typename T>
 void bubble_sort(vector<T> &v) {
 	for (int i = 0; i < v.size()-1; i++) {
@@ -12,6 +14,30 @@ void bubble_sort(vector<T> &v) {
 	}
 }
 
+
+//øÏÀŸ≈≈–Ú
+template<typename T>
+T Partition(vector<T>& v, T low, T high) {
+	T temp = v[low];
+	while (low < high) {
+		while (low < high && v[high] >= temp)high--;
+		v[low] = v[high];
+		while (low < high && v[low] <= temp)low++;
+		v[high] = v[low];
+	}
+	v[low] = temp;
+	return low;
+}
+template<typename T>
+void quick_sort(vector<T>& v,T low,T high) {
+	if (low < high) {
+		T p = Partition(v, low, high);
+		quick_sort(v, low, p - 1);
+		quick_sort(v, p + 1, high);
+	}
+}
+
+//÷ÿ‘ÿ<<
 template<typename T>
 ostream& operator<<(ostream& out, const vector<T>&v) {
 	out << "{";
@@ -31,7 +57,13 @@ int main()
 	cout << "base:"<< endl;
 	vector<int>A = { 3,1,4,2,5 };
 	cout << A << endl;
+
 	cout << "bubble_sort:" << endl;
 	bubble_sort(A);
+	cout << A << endl;
+
+	A = { 3,1,4,2,5 };
+	cout << "quick_sort:" << endl;
+	quick_sort(A,0,int(A.size()-1));
 	cout << A << endl;
 }
